d6a355f546f0f56a965956e8a72f9226a60c1ca0
andruraj <andrewraj10@gmail.com>
2019-07-11 21:17:24 +0530

    Updates

---
+++ precache-manifest.c6d506dec5234d897eda418c228c2c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41a21ccdc1a471d3232c53f2964d864",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "60503e996bff8ef492ea",
+    "url": "/weather/static/css/main.0ded14b4.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/weather/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "60503e996bff8ef492ea",
+    "url": "/weather/static/js/main.a96ba520.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
